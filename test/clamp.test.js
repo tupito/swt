fbb8267fe0b0eaef70c379b4dcde45d2d5d494aa
Janne Ruohoniemi <janne.ruohoniemi@tuni.fi>
2024-11-07 21:30:50 +0200

    test: fix incorrect parameters for clamp.js

--- test/clamp.test.js
+++ test/clamp.test.js
@@ -11,22 +11,22 @@ describe("clamp.js", () => {
 
     it('should handle too low value (#TC07)', () => {
         // This should be the correct way:
-        expect(clamp(-1, 0, 100)).to.equal(0);
+        //expect(clamp(-1, 0, 100)).to.equal(0);
         // Due to the bug in the function, it should be called:
-        //expect(clamp(0, 100, -1)).to.equal(0);
+        expect(clamp(0, 100, -1)).to.equal(0);
     });
 
     it('should handle correct value (#TC08)', () => {
         // This should be the correct way:
-        expect(clamp(50, 0, 100)).to.equal(50);        
+        //expect(clamp(50, 0, 100)).to.equal(50);        
         // Due to the bug in the function, it should be called:        
-        //expect(clamp(0, 100, 50)).to.equal(50);        
+        expect(clamp(0, 100, 50)).to.equal(50);        
     });
 
     it('should handle too high value (#TC09)', () => {
         // This should be the correct way:
-        expect(clamp(150, 0, 100)).to.equal(100);
+        //expect(clamp(150, 0, 100)).to.equal(100);
         // Due to the bug in the function, it should be called:
-        //expect(clamp(0, 100, 150)).to.equal(100);
+        expect(clamp(0, 100, 150)).to.equal(100);
     });
 });
